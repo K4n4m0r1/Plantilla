39b1a1f87277e559019a3c16455c816467b48b75
Tu Nombre <jaureguichilla@gmail.com>
2023-08-11 17:53:48 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Web</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="nav">
+            <div class="logo"><img src="https://www.ccapasistemas.com/assets/images/logo1.png"></div>
+            <ul>
+                <li>Item1</li>
+                <li>item2</li>
+                <li>Item3</li>
+                <li>Item4</li>
+                <li>Item5</li>
+                <li>Item6</li>
+            </ul>
+        </div>
+    </div>
+<section>
+    <section class="banner">
+        <p>Soluciones <br>
+        tecnológicas a <br>
+        pequeñasy medianas <br>
+        empresas!</p>
+        <span>Más de 12 años de experiencia, personalizamos cada <br>
+        proyecto según las necesidades de nuestros clientes.</span><br>
+
+        <button class="conocenos">
+            Conócenos
+        </button>
+    </section>
+    <secttion class="descripcion">
+       
+        <div class="texto1">
+        <h2>Brindamos Soluciones de <br>
+        Software Empresarial.</h2>
+            <div class="descripcion2"> Considere nuestra empresa como un socio tecnológico y repotencie su negocio. Estamos especializados en Tecnologías Web por lo que contamos con ofertas integrales de soluciones propias y servicios avanzados de alto valor e innovación. Somos el socio que las empresas buscan para ser apoyadas en sus estrategias de negocios por medio de la tecnología. Por estas razones nuestras soluciones en consultoría y tecnología han logrado desarrollar e impulsar las operaciones clave de los negocios de cientos de emprendedores a nivel nacional.
+            </div>
+            <div class="banner2">
+                <img src="" alt="Like">
+                    <h3>Cómodos Planes</h3>
+                    <p>Ajustamos y optimizamos nuestros planes según tu presupuesto y solicitudes.</p>
+            </div>
+            <div class="banner3">
+                <img src="" alt="Like">
+                    <h3>Desarrollo a Medidas</h3>
+                    <p>Desarrollamos tu sistema de software según tus necesidades</p>
+
+            </div>
+        </div>
+        <div class="imagen1">
+            <img src="image/about.jpg" alt="">
+        </div>
+    </secttion>
+</section>
+
+    
+</body>
+</html>
